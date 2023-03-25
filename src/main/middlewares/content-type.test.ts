7361fa1e6cbd762fc128765c509b67b3dd9dd01a
Hernany Motoso <contatohernany@gmail.com>
2023-03-25 10:15:37 -0300

    refactor: group middleware interfaces  in a single file

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest'
-import app from '../app'
+import app from '../config/app'
 
 describe('Content Type Middleware', () => {
   it('Should return default content type as json', async () => {
