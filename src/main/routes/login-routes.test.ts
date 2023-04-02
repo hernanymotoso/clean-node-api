b4033c75530c91eac33b5679e19538721e3637f0
Hernany Motoso <contatohernany@gmail.com>
2023-04-02 20:27:41 -0300

    refactor: rename signup routes with login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
-describe('SignUp Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -16,14 +16,16 @@ describe('SignUp Routes', () => {
     await accountCollection.deleteMany({})
   })
 
-  it('Should return a account on success ', async () => {
-    await request(app).post('/api/signup')
-      .send({
-        name: 'Hernany',
-        email: 'hernanymotoso@gmail.com',
-        password: '123',
-        passwordConfirmation: '123'
-      })
-      .expect(200)
+  describe('POST /signup', () => {
+    it('Should return 200 on signup ', async () => {
+      await request(app).post('/api/signup')
+        .send({
+          name: 'Hernany',
+          email: 'hernanymotoso@gmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
   })
 })
