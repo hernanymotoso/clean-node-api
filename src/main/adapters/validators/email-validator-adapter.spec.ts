cdb984b897f8506ec1628da0712d920ac3c16aa9
Hernany Motoso <contatohernany@gmail.com>
2023-04-20 08:52:53 -0300

    chore: code cleanup

--- src/main/adapters/validators/email-validator-adapter.spec.ts
+++ src/main/adapters/validators/email-validator-adapter.spec.ts
@@ -16,14 +16,12 @@ describe('EmailValidator Adapter', () => {
     const sut = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@email.com')
-
     expect(isValid).toBe(false)
   })
 
   it('Should return true if validator returns true', () => {
     const sut = makeSut()
     const isValid = sut.isValid('valid_email@email.com')
-
     expect(isValid).toBe(true)
   })
 
@@ -31,7 +29,6 @@ describe('EmailValidator Adapter', () => {
     const sut = makeSut()
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
     sut.isValid('any_email@email.com')
-
     expect(isEmailSpy).toHaveBeenCalledWith('any_email@email.com')
   })
 })
