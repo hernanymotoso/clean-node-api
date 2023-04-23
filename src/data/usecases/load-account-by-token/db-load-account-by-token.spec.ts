69eeeb9d842fda5c0ecd26377deaf8cbfe2c2c47
Hernany Motoso <contatohernany@gmail.com>
2023-04-23 01:10:16 -0300

    test: ensure DbLoadAccountByToken returns an account on success

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -73,4 +73,10 @@ describe('DbLoadAccountbyToken Usecase', () => {
     const account = await sut.load('any_token', 'any_role')
     expect(account).toBeNull()
   })
+
+  it('Should return an account on success', async () => {
+    const { sut } = makeSut()
+    const account = await sut.load('any_token', 'any_role')
+    expect(account).toEqual(makeFakeAccount())
+  })
 })
