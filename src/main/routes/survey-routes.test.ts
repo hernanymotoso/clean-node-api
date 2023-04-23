024cae9c3bb069782f01cb1d3bfcd510351f7478
Hernany Motoso <contatohernany@gmail.com>
2023-04-23 12:38:13 -0300

    test: ensure add survey route returns 403 if no accessToken is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -20,7 +20,7 @@ describe('Survey Routes', () => {
   })
 
   describe('POST /surveys', () => {
-    it('Should return 204 on add survey success ', async () => {
+    it('Should return 403 on add survey without accessToken', async () => {
       await request(app).post('/api/surveys')
         .send({
           question: 'Question',
@@ -31,7 +31,7 @@ describe('Survey Routes', () => {
             answer: 'Answer 2'
           }]
         })
-        .expect(204)
+        .expect(403)
     })
   })
 })
