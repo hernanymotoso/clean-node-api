80e74114f239303a1a4f5b0c9862146e9a18884d
Hernany Motoso <contatohernany@gmail.com>
2023-04-26 11:15:53 -0300

    test: ensure LoadSurveysController returns 204 if there is no conteant

--- src/presentation/controllers/survey/load-surveys/load-surveys.spec.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys.spec.ts
@@ -1,7 +1,7 @@
 import { LoadSurveysController } from './load-surveys-controller'
 import { LoadSurveys, SurveyModel } from './load-surveys-controller-protocols'
 import MockDate from 'mockdate'
-import { ok, serverError } from '../../../helpers/http/http-helper'
+import { ok, serverError, noContent } from '../../../helpers/http/http-helper'
 
 const makeFakeSurveys = (): SurveyModel[] => {
   return [{
@@ -49,7 +49,7 @@ describe('LoadSurveys Controller', () => {
     MockDate.set(new Date())
   })
 
-  beforeAll(() => {
+  afterAll(() => {
     MockDate.reset()
   })
 
@@ -66,9 +66,18 @@ describe('LoadSurveys Controller', () => {
     expect(httpResponse).toEqual(ok(makeFakeSurveys()))
   })
 
+  it('Should return 204 if LOadSurveys return empty ', async () => {
+    const { sut, loadSurveysStub } = makeSut()
+    jest.spyOn(loadSurveysStub, 'load')
+      .mockReturnValueOnce(new Promise((resolve) => resolve([])))
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(noContent())
+  })
+
   it('Should return 500 if LoadSurveys throws', async () => {
     const { sut, loadSurveysStub } = makeSut()
-    jest.spyOn(loadSurveysStub, 'load').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    jest.spyOn(loadSurveysStub, 'load')
+      .mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(serverError(new Error()))
   })
